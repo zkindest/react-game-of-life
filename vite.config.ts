f911e917b9156b2d1bdbed0ad60835c7e8f81fba
zkindest <zkindest5@gmail.com>
2022-01-15 20:42:43 +0530

    fix basename in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fe-c7-bookmark-landing-page/'
+  base: '/react-game-of-life/'
 })
