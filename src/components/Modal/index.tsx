5b74ee99e74a79964d91d6427a601a9772d2cc7e
zkindest <zkindest5@gmail.com>
2022-01-16 20:02:24 +0530

    add modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,64 @@
+// ref: https://www.w3.org/TR/wai-aria-practices-1.1/examples/dialog-modal/js/dialog.js
+import { HTMLAttributes, ReactNode, RefObject, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import defaultStyles from './index.module.css';
+import useModal from "../../hooks/useModal";
+
+
+interface ModalProps extends HTMLAttributes<HTMLDivElement> {
+  focusFirst?: string | RefObject<HTMLElement> | HTMLElement;
+  focusAfterClosed?: string | RefObject<HTMLElement> | HTMLElement;
+  autoFocus?: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  maxWidth?: string;
+  classes?: {
+    root?: string
+    backDrop?: string
+    childrenOuter?: string
+    childrenContainer?: string
+  }
+}
+const Modal = (
+  { focusFirst, focusAfterClosed, onClose, autoFocus, children, maxWidth, classes, ...rest }: ModalProps
+) => {
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true)
+    return () => {
+      setIsMounted(false)
+    }
+  }, [isMounted])
+
+  const { ref, modalRoot } = useModal<HTMLDivElement>({ autoFocus, focusFirst, focusAfterClosed, onClose, overlayModal: true });
+
+  const modal = (
+    <div role="presentation" className={`${defaultStyles.modal} ${classes ? classes?.root : ''}`}>
+      <div className={`${defaultStyles.backdrop} ${isMounted ? defaultStyles.mounted : ''} ${classes ? classes?.backDrop : ''}`}>
+        <div tabIndex={0}></div>
+        <div
+          role="presentation"
+          tabIndex={-1}
+          className={`${defaultStyles.children__outer} ${classes?.childrenOuter ? classes.childrenOuter : ''}`}
+        >
+          <div
+            className={`${defaultStyles.children__container} ${classes?.childrenContainer ? classes.childrenContainer : ''}`}
+            ref={ref}
+            role="dialog"
+            style={maxWidth ? { maxWidth } : {}}
+            {...rest}
+          >
+            {children}
+          </div>
+        </div>
+        <div tabIndex={0}></div>
+      </div>
+    </div>
+  );
+
+  return ReactDOM.createPortal(modal, modalRoot);
+};
+
+export default Modal;
---
+++ src/hooks/useModal.ts
@@ -0,0 +1,159 @@
+import { RefObject, useEffect, useRef } from "react";
+
+
+const getDOMRef = (value: string | RefObject<HTMLElement> | HTMLElement): HTMLElement | null => {
+  if (typeof value == "string") {
+    return document.querySelector(value);
+  } else if (typeof value == "object") {
+    if (value instanceof HTMLElement) return value;
+    if (value.current) return value.current;
+  }
+  return null
+};
+interface UseModalProps {
+  focusFirst?: string | RefObject<HTMLElement> | HTMLElement;
+  focusAfterClosed?: string | RefObject<HTMLElement> | HTMLElement;
+  autoFocus?: boolean;
+  onClose: () => void;
+  overlayModal?: boolean;
+}
+
+const useModal = <RefType extends HTMLElement>({ focusFirst, onClose, focusAfterClosed, autoFocus, overlayModal = true }: UseModalProps) => {
+
+  let modalRoot = document.getElementById("modal-root");
+  if (!modalRoot) {
+    modalRoot = document.createElement("div");
+    modalRoot.setAttribute("id", "modal-root");
+    document.body.appendChild(modalRoot);
+  }
+
+  const ref = useRef<RefType | null>(null);
+
+  let _focusFirst = focusFirst ? getDOMRef(focusFirst) : null;
+  let _focusAfterClosed = focusAfterClosed ? getDOMRef(focusAfterClosed) : null;
+
+  // attach necessary click and key event handlers 
+  useEffect(() => {
+    function handleOutsideClick(e: MouseEvent) {
+      if (!ref.current) {
+        return
+      }
+      if (!ref.current.contains(e.target as HTMLElement)) {
+        onClose();
+        if (_focusAfterClosed) _focusAfterClosed.focus();
+      }
+    }
+
+    function handleKeyPress(e: KeyboardEvent) {
+      if (e.key === 'Escape') {
+        onClose()
+        if (_focusAfterClosed) _focusAfterClosed.focus();
+      }
+    }
+    document.body.addEventListener('keyup', handleKeyPress)
+    document.body.addEventListener('click', handleOutsideClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick, { capture: true });
+      document.body.removeEventListener('keyup', handleKeyPress)
+    }
+  }, [onClose, _focusAfterClosed])
+
+  // attach focus event handlers to handle tabbing and what to focus on open/close modal
+  useEffect(() => {
+    const isFocusable = (element: HTMLElement): element is HTMLElement => {
+      return typeof element.focus === "function";
+    };
+
+    // `focus` event can be triggered by keyboard(user input),javascript
+    // we want to run some operations only when focus is triggered by keyboard
+    let ignoreUntilFocusChanges = false;
+    const attempFocus = (element: HTMLElement) => {
+      if (!isFocusable(element)) {
+        return false;
+      }
+      ignoreUntilFocusChanges = true;
+
+      try {
+        element.focus();
+      } catch (e) {
+        console.error(e);
+      }
+      ignoreUntilFocusChanges = false;
+      return document.activeElement === element;
+    };
+    const focusFirstDescendant = (element: HTMLElement) => {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        var child = element.childNodes[i];
+
+        if (attempFocus(child as HTMLElement) || focusFirstDescendant(child as HTMLElement)) {
+          return true;
+        }
+      }
+      return false;
+    };
+    const focusLastDescendant = (element: HTMLElement) => {
+      for (var i = element.childNodes.length - 1; i >= 0; i--) {
+        var child = element.childNodes[i];
+        if (attempFocus(child as HTMLElement) || focusLastDescendant(child as HTMLElement)) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    if (_focusFirst) _focusFirst.focus();
+    else {
+      if (autoFocus && ref.current) {
+        focusFirstDescendant(ref.current);
+      }
+    }
+
+    let lastFocus: any;
+    const trapFocus = (e: FocusEvent) => {
+
+      if (ignoreUntilFocusChanges) {
+        return;
+      }
+
+      if (!ref.current) {
+        console.error("dialog not found");
+        return;
+      }
+      if (ref.current.contains(e.target as HTMLElement)) {
+        lastFocus = e.target;
+      } else {
+
+        focusFirstDescendant(ref.current);
+
+        // user clicks Shift + Tab when activeElement is first focusable descendant inside dialog,in this case `focusFirstDescendant` won't change lastFocus
+        // then focus last descendant, it goes round and round
+        if (lastFocus === document.activeElement) {
+          focusLastDescendant(ref.current);
+        }
+
+        lastFocus = document.activeElement;
+      }
+    };
+
+    if (overlayModal) {
+      document.body.classList.add("has-dialog");
+    }
+    document.addEventListener("focus", trapFocus, true);
+
+    return () => {
+      if (overlayModal) {
+        document.body.classList.remove("has-dialog");
+      }
+      document.removeEventListener("focus", trapFocus, true);
+
+      if (_focusAfterClosed) {
+        _focusAfterClosed.focus()
+      }
+    };
+  }, [_focusFirst, _focusAfterClosed, autoFocus, overlayModal]);
+
+  return { ref, modalRoot };
+};
+
+export default useModal;
