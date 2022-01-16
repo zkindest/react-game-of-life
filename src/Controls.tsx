28d43a056f0cf6de860d9498154ca6f4d66b5f6b
zkindest <zkindest5@gmail.com>
2022-01-16 20:07:06 +0530

    modify controls with new components

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,5 +1,11 @@
-import { ChangeEvent, Dispatch } from "react";
+import { ChangeEvent, Dispatch, useState } from "react";
+import Button from "./components/Button";
+import { PauseSVG, PlaySVG, Random, Reset, StepForwardSVG } from "./components/Icons";
 import { GameBoardAction } from "./types";
+import Switch from './components/Switch';
+import Select from "./components/Select";
+
+const gpsOptions = ['1', '5', '10', '15', '20'];
 
 interface ControlProps {
   genCount: number;
@@ -16,13 +22,17 @@ const Controls = ({
   dispatch
 }: ControlProps) => {
 
+  const [auto, setAuto] = useState(true);
+
   const handleGPSChange = (e: ChangeEvent<HTMLSelectElement>) => {
     // handles regeneration interval change
     dispatch({ type: 'gps-change', payload: Number(e.target.value) })
   };
 
   const handleAutoChange = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch({ type: 'auto-option-change', payload: e.target.checked })
+    const nextValue = !auto;
+    setAuto(nextValue);
+    dispatch({ type: 'auto-option-change', payload: nextValue })
   }
   const handleStep = () => {
     dispatch({ type: "step-next" });
@@ -44,29 +54,35 @@ const Controls = ({
       </div>
       {
         isAuto ? (
-          <button onClick={handlePlay} className="button" title="toggle play">
-            {isPlaying ? "⏸" : '▶'}
-          </button>
+          <Button onClick={handlePlay} className="button" title="toggle play">
+            {isPlaying ? <PauseSVG /> : <PlaySVG />}
+          </Button>
         ) : (
-          <button className="button" title="step forward once" onClick={handleStep}>⏭</button>
+          <Button className="button" title="step forward once" onClick={handleStep} >
+            {<StepForwardSVG />}
+          </Button>
         )
       }
-      <div className="select" title="control generations per second">
-        <label htmlFor="fps">gps:</label>
-        <select value={generationsPerSecond} onChange={handleGPSChange}>
-          <option value="1">1</option>
-          <option value="5">5</option>
-          <option value="10">10</option>
-          <option value="15">15</option>
-          <option value="20">20</option>
-        </select>
-      </div>
-      <div className="checkbox" title="auto generate cells at selected gps">
-        <label htmlFor="auto">Auto</label>
-        <input type="checkbox" id="auto" defaultChecked onChange={handleAutoChange} />
-      </div>
-      <button id="reset" className="button" title="kill all cells and reset generations to zero" onClick={handleReset}>🔄</button>
-      <button id="random" className="button" title="randomly generate cells" onClick={handleRandomGeneration}>🔀</button>
+      <Button id="reset" className="button" title="kill all cells and reset generations to zero" onClick={handleReset} >
+        <Reset />
+      </Button>
+      <Button id="random" className="button" title="randomly generate cells" onClick={handleRandomGeneration}>
+        <Random />
+      </Button>
+      <Select
+        label="gps"
+        options={gpsOptions}
+        onChange={handleGPSChange}
+        value={generationsPerSecond}
+        aria-label="control generations per second"
+      />
+      <Switch
+        name="auto or manual"
+        title="auto generate cells at selected gps"
+        options={["auto", "manual"]}
+        onChange={handleAutoChange}
+        currentOption={auto ? 'auto' : 'manual'}
+      />
     </div>
   )
 }
